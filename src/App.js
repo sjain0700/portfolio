42f64e1b841bdb8dd18cb0bf60d81840cd62650a
Sunil Kumar Jain <v-sunil@damacgroup.com>
2025-07-19 18:13:04 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import TechnicalSkill from './components/TechnicalSkill';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <TechnicalSkill />
+      <Projects />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
